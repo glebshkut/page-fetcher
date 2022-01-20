d0e819c13416798892f19ee57a370d14b69d8f43
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-19 21:45:08 -0800

    edges refactored

--- fetcher.js
+++ fetcher.js
@@ -5,13 +5,17 @@ const args = process.argv.slice(2);
 
 
 const content = request(args[0], (error, response, body) => {
+  if (error !== null) {
+    console.log(error);
+    return;
+  }
   fs.writeFile(args[1], body, err => {
     if (err) {
       console.error(err)
       return
     }
+    countBytes(body);
   });
-  countBytes(body);
 });
 
 const countBytes = (item) => {
